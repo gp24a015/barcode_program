348d21767a3e0f515a00e1faf0c8339da1924b1f
GP24A015 <gp24a015@oecu.jp>
2026-08-25 05:25:00 +0900

    Vercel APIの動作確認

--- api/search.py
+++ api/search.py
@@ -1,99 +1,16 @@
-import os
-import json
-from urllib.request import urlopen
-from urllib.error import URLError
-
-APPID = os.environ["APPID"]
+from http.server import BaseHTTPRequestHandler
 
 
-def jancode_to_name(code):
-    url = (
-        "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
-        f"?appid={APPID}&jan_code={code}&results=1"
-    )
+class handler(BaseHTTPRequestHandler):
 
-    try:
-        with urlopen(url) as resp:
-            res = json.load(resp)
-    except URLError:
-        return None
-
-    if "hits" in res and res["hits"]:
-        return res["hits"][0]["name"]
-
-    return None
-
-
-def handler(request):
-    if request.method != "POST":
-        return {
-            "statusCode": 405,
-            "headers": {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            "body": json.dumps(
-                {"error": "POST only"},
-                ensure_ascii=False
-            )
-        }
-
-    try:
-        data = request.body
-        if isinstance(data, bytes):
-            data = json.loads(data)
-        elif isinstance(data, str):
-            data = json.loads(data)
-
-    except Exception:
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            "body": json.dumps(
-                {"error": "JSONの形式が正しくありません"},
-                ensure_ascii=False
-            )
-        }
-
-    barcode = data.get("barcode")
-
-    if not barcode:
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            "body": json.dumps(
-                {"error": "JANコードがありません"},
-                ensure_ascii=False
-            )
-        }
-
-    product_name = jancode_to_name(barcode)
-
-    if product_name is None:
-        return {
-            "statusCode": 404,
-            "headers": {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            "body": json.dumps(
-                {"error": "商品が見つかりません"},
-                ensure_ascii=False
-            )
-        }
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        "body": json.dumps(
-            {
-                "barcode": barcode,
-                "product_name": product_name
-            },
-            ensure_ascii=False
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header(
+            "Content-type",
+            "text/plain; charset=utf-8"
         )
-    }
+        self.end_headers()
+
+        self.wfile.write(
+            "APIは正常に動作しています".encode("utf-8")
+        )
